ba8a257b9dadd8bb67de2be5a6d786a28a5a9a18
GogumaYummy <sora1211x@gmail.com>
2023-01-05 15:14:14 +0900

    d

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
 app
   .get('/', (req, res) => {
     res.send('bye');
+  })
+  .get('/mkdir', (req, res) => {
+    fs.mkdir('./dd');
+    res.send('good');
   })
   .listen(3000, () => {
     console.log('Listening on port 3000.');
