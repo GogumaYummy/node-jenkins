4be017a7093b623d1c427263b7ce921b708d91d5
GogumaYummy <sora1211x@gmail.com>
2023-01-05 15:00:11 +0900

    fdg

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app
   .get('/', (req, res) => {
-    res.send('hihihihihi');
+    res.send('bye');
   })
   .listen(3000, () => {
     console.log('Listening on port 3000.');
